edea59c81900eaf25e0ab771d1da0fedb2442e99
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-30 16:03:21 -0500

    setup quiz question class

--- src/com/company/QuizQuestion.java
+++ src/com/company/QuizQuestion.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class QuizQuestion {
+    String question;
+    String answer;
+
+    public QuizQuestion(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
 }
