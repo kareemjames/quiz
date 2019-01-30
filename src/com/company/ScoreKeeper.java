8e1ba069be8fe5c9c43117c72e7ac176d67c446e
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-30 17:37:39 -0500

    add score to scorekeeper class

--- src/com/company/ScoreKeeper.java
+++ src/com/company/ScoreKeeper.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class ScoreKeeper {
+    private int score = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
