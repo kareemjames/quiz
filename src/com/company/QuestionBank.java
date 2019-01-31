4493744ed94426985ad1c1a87e4e1afb8b2dc9b9
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-30 19:01:50 -0500

    add methods to add question and answer

--- src/com/company/QuestionBank.java
+++ src/com/company/QuestionBank.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class QuestionBank {
+    private ArrayList<String> questions = new ArrayList<>();
+    private ArrayList<String> answers = new ArrayList<>();
+
+    public void addQuestion(String x) {
+        questions.add(x);
+    }
+
+    public void addAnswer(String x) {
+        answers.add(x);
+    }
+
+
 }
